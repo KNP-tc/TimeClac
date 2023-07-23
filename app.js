d708d6add0fabccedc98077ff8a8a2dc13f4c521
Z TC <68360172+KNP-tc@users.noreply.github.com>
2023-07-23 08:54:59 +0700

    fdsafsd

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Use EJS as the templating engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '.'));  // change this if your views are in a different folder
+app.set('views', path.join(__dirname, 'views'));  // change this if your views are in a different folder
 
 // Enable parsing of request body
 app.use(bodyParser.urlencoded({ extended: false }));
