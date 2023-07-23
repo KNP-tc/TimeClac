7785ff7104bf413e3bda34932915be6d391ddb1d
Z TC <68360172+KNP-tc@users.noreply.github.com>
2023-07-23 08:05:53 +0700

    Fix some issue

--- app.js
+++ app.js
@@ -31,4 +31,4 @@ app.post('/', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
